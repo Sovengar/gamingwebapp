2280a2a144797136891a1f9a0e620e1361df547c
Sovengar <bubles_2009@hotmail.com>
2020-05-11 04:18:56 +0200

    First stable local version

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamingwebapp.wsgi --log-file -
---
+++ appsite/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    context = {
+        "none": 1
+    }
+    return render(request, "appsite/index.html", context)
