e48ee47323038141fdd9f31172c8816f91e7c3b1
Sovengar <bubles_2009@hotmail.com>
2020-05-12 05:09:31 +0200

    building auth feature

--- appsite/urls.py
+++ appsite/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("signin", views.sig_in, name="sign_in"),
-    path("signup", views.sig_up, name="sign_up")
+    path("", views.index_view, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
+    path("logout/", views.logout_view, name="logout")
 ]
 
--- appsite/views.py
+++ appsite/views.py
@@ -1,23 +1,34 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth import logout, login, authenticate
 
 # Create your views here.
 
-def index(request):
+def index_view(request):
+    if not request.user.is_authenticated:
+        return render(request, "appsite/login.html", {"message": None})
     context = {
-        "none": 1
+        "user": request.user
     }
-    return render(request, "appsite/index.html", context)
+    return render(request, "appsite/user.html", context)
 
-def sig_in(request):
+def register_view(request):
     context = {
         "none": 1
     }
-    return render(request, "appsite/modules_sign_in.html", context)
+    return render(request, "appsite/register.html", context)
 
-def sig_up(request):
-    context = {
-        "none": 1
-    }
-    return render(request, "appsite/modules_sign_up.html", context)
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "appsite/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "appsite/login.html", {"message": "Logged out."})
