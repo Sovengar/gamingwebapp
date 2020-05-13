68c8993f99c58ed1642f12f02f275e67a05d21cb
Sovengar <bubles_2009@hotmail.com>
2020-05-13 03:08:32 +0200

    fix settings.py

--- gamingwebapp/settings.py
+++ gamingwebapp/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '9$**_c%z22nnaujbag^%ktn_#+c!&d_eban+z$7mxg@jx%be3='
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '9$**_c%z22nnaujbag^%ktn_#+c!&d_eban+z$7mxg@jx%be3='
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+#DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['gamingwebapp.herokuapp.com']
 
